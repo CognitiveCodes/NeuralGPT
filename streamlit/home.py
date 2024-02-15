5dd05efdb16023b95571832cdea73268764f3923
CognitiveCodes <133844350+CognitiveCodes@users.noreply.github.com>
2024-02-15 19:24:36 +0100

    Update home.py

--- streamlit/home.py
+++ streamlit/home.py
@@ -1,5 +1,9 @@
 import asyncio
 import streamlit as st
+import http.server
+import socketserver
+
+PORT = 8000
 
 servers = {}
 inputs = []
@@ -8,40 +12,58 @@ used_ports = []
 server_ports = []
 client_ports = []
 
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
 st.set_page_config(layout="wide")
 
-st.session_state.update(st.session_state)
-
-if 'active_page' not in st.session_state:
-    st.session_state.active_page = 'NeuralGPT'
-if "sidebar" not in st.session_state:
-    st.session_state.sidebar = True
-if "server_Ports" not in st.session_state:
-    st.session_state['server_ports'] = False
-if "client_Ports" not in st.session_state:
-    st.session_state['client_ports'] = False
-if "userID" not in st.session_state:
-    st.session_state.userID = False
-
-st.sidebar.title("NeuralGPT")
-st.sidebar.info(
-    """
-    [GitHub](https://github.com/giswqs)
-    """
-)
-
 async def main():
-    st.text("Server ports:")
+    
+    st.session_state.update(st.session_state)
+
+    if "server_ports" not in st.session_state:
+        st.session_state['server_ports'] = ""
+    if "client_ports" not in st.session_state:
+        st.session_state['client_ports'] = ""
+    if "user_ID" not in st.session_state:
+        st.session_state.user_ID = ""
+    if "gradio_Port" not in st.session_state:
+        st.session_state.gradio_Port = "" 
+    if "servers" not in st.session_state:
+        st.session_state.servers = None
+    if "server" not in st.session_state:
+        st.session_state.server = None    
+    if "clients" not in st.session_state:
+        st.session_state.clients = None    
+    if "api_key" not in st.session_state:
+        st.session_state.api_key = None
+    if "tokenChar" not in st.session_state:
+        st.session_state.tokenChar = None              
+    if "charName" not in st.session_state:
+        st.session_state.charName = None
+    if "character_ID" not in st.session_state:
+        st.session_state.character_ID = None 
+
+    if  st.session_state.server == None:
+        st.session_state.active_page = 'clients'
+    else: st.session_state.active_page = 'servers'
+
+    st.title("NeuralGPT")
+        
+    st.sidebar.text("Gradio app")
+    gradio_Ports = st.container(border=True)
+    gradio_Ports.markdown(st.session_state.gradio_Port)    
     serverPorts = st.sidebar.container(border=True)
     serverPorts.markdown(st.session_state['server_ports'])
     st.sidebar.text("Client ports")
     clientPorts = st.sidebar.container(border=True)
     clientPorts.markdown(st.session_state['client_ports'])
     st.sidebar.text("Charavter.ai ID")
-    user_id = st.sidebar.container(border=True)
-    user_id.markdown(st.session_state.userID)
 
-    st.title("NeuralGPT")
+     st.title("NeuralGPT")
 
     st.markdown(
         """
@@ -74,5 +96,7 @@ async def main():
         st.image("https://i.postimg.cc/T1sdWCL2/pyth.png")
         st.image("https://i.postimg.cc/L8T5s9Gk/bandicam2023-02-0323-10-40-545-ezgif-com-speed.gif")
         st.image("https://i.postimg.cc/rF5zvCJP/clocks5.gif")
+    user_id = st.sidebar.container(border=True)
+    user_id.markdown(st.session_state.user_ID)
 
-asyncio.run(main())    
+asyncio.run(main())
